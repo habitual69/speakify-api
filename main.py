b061e6225ff8e119e395f6a9a2ffd1bcb531860b
habitual69 <admin@vpms.xyz>
2024-07-09 10:30:10 +0530

    cors added

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router as api_router
 
 app = FastAPI(title="Speakify", version="1.0.0")
+
+# Add CORS middleware
+origins = [
+"*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
 
